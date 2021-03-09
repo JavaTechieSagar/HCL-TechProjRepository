1ca393c0c24276106caa0675b21970c8051a9562
JavaTechieSagar <sagar4java2020@gmail.com>
2021-03-09 17:22:09 +0530

    Version 1.1 of HelloWorld - by Sagar @17:20pm

--- Desktop/Today/HCL/Workspace/HelloWorldApp/src/HelloWorld.java
+++ Desktop/Today/HCL/Workspace/HelloWorldApp/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World! - Version 1.0");
+		System.out.println("Hello World! - Version 2.0");
 			
 	}
 
