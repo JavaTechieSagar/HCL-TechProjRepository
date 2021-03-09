2ddecbbeda32564bf5853188881ff084ca4406b0
JavaTechieSagar <76060234+JavaTechieSagar@users.noreply.github.com>
2021-03-09 17:41:55 +0530

    Update HelloWorld.java

--- Desktop/Today/HCL/Workspace/HelloWorldApp/src/HelloWorld.java
+++ Desktop/Today/HCL/Workspace/HelloWorldApp/src/HelloWorld.java
@@ -6,6 +6,11 @@ public class HelloWorld {
 		
 		System.out.println("Hello World! - Version 1.0");
 		System.out.println("Hello World! - Version 2.0");
+		/*
+		   Add an ArrayList Object that contains 10 products
+		   TODO - Read the data of all products and send the highest priced product details  maxProductShow() method
+		
+		*/
 			
 	}
 
